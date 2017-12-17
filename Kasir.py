a36d7a15c6c3a4bd5c8d4341669d4ece37fff25c
mayoo23 <sandidwi367@gmail.com>
2017-12-17 15:01:31 +0700

    blank

--- Kasir.py
+++ Kasir.py
@@ -5,3 +5,8 @@ jumlah_barang = int(input("Masukkan jumlah barang = "))
 total = x*jumlah_barang
 print("Total Harga", nama, "Anda adalah Rp.",total)
 pembayaran = int(input("Tunai = "))
+if(pembayaran>total):
+	print("===========================Terimakasih telah datang ke Toko kami===============")
+	print("kembali Rp.", pembayaran-total)
+else:
+	print("Anda memiliki kekurangan sebesar Rp.",kekurangan)
