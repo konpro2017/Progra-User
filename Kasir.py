a1ce5ca85314d8da25b0670c3aa5fb18772074a8
mayoo23 <sandidwi367@gmail.com>
2017-12-17 14:24:07 +0700

    koma

---
+++ Kasir.py
@@ -0,0 +1,7 @@
+print ("==============================Toko Serba Ada======================================")
+nama = input("Masukkan Nama Barang : ")
+x = int(input("Masukkan Harga Barang : "))
+jumlah_barang = int(input("Masukkan jumlah barang = "))
+total = x*jumlah_barang
+print("Total Harga", nama, "Anda adalah Rp.",total)
+pembayaran = int(input("Tunai = "))
