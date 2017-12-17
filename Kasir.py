801b0ac7d251b55775da69699f3d8b82b505db0d
lindoa12 <34624025+lindoa12@users.noreply.github.com>
2017-12-17 22:29:16 +0700

    Add files via upload

--- Kasir.py
+++ Kasir.py
@@ -1,10 +1,13 @@
 print ("==============================Toko Serba Ada======================================")
+
 nama = input("Masukkan Nama Barang : ")
 x = int(input("Masukkan Harga Barang : "))
 jumlah_barang = int(input("Masukkan jumlah barang = "))
-total = x*jumlah_barang
+berat_barang = int(input("Masukkan berat barang = "))
+total = x*jumlah_barang*berat_barang
 print("Total Harga", nama, "Anda adalah Rp.",total)
 pembayaran = int(input("Tunai = "))
+kekurangan = total-pembayaran
 if(pembayaran>total):
 	print("===========================Terimakasih telah datang ke Toko kami===============")
 	print("kembali Rp.", pembayaran-total)
