b3196ec5d0f7fb447a1768bcb696111f4837cf4b
Puspitasari12 <Renidiyahsari@gmail.com>
2017-12-17 23:07:12 +0700

    enter

--- Kasir.py
+++ Kasir.py
@@ -1,7 +1,4 @@
 print ("==============================Toko Serba Ada======================================")
-penjual = input("Nama Penjual : ")
-print("===============================Selamat datang ditoko Bapak Udin====================")
-pembeli = input("Nama Pembeli : ")
 nama = input("Masukkan Nama Barang : ")
 x = int(input("Masukkan Harga Barang : "))
 jumlah_barang = int(input("Masukkan jumlah barang = "))
@@ -11,7 +8,6 @@ print("Total Harga", nama, "Anda adalah Rp.",total)
 pembayaran = int(input("Tunai = "))
 kekurangan = total-pembayaran
 if(pembayaran>total):
-	print("===========================Terimakasih telah datang ke Toko kami===============")
 	print("kembali Rp.", pembayaran-total)
 else:
 	print("Anda memiliki kekurangan sebesar Rp.",kekurangan)
